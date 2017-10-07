44f758de681fe3f16361128ec2943896e089f0b3
Brottod <jan@jan-laptop.localdomain>
2017-10-07 18:24:22 +0200

    neki

--- jan/readdata.py
+++ jan/readdata.py
@@ -5,10 +5,9 @@ from datetime import datetime
 import numpy as np
 import re
 
-
 arr = []
 
-with open("../Data/Simul/Sim1.csv") as dat_f:
+with open("../Data/Simul/Sim18.csv") as dat_f:
     reader = csv.DictReader(dat_f, delimiter=";")
     for row in reader:
         arr.append(row)
@@ -32,7 +31,7 @@ for i,line in enumerate(arr):
 # plt_keys = ["N1_au", "N2_au", "N10_au", "N15_au"]
 
 
-regex_vzorec = ["N.*_u", "N.*_au", "N.*_r", "N.*_P.*", "N.*_Q.*"]
+regex_vzorec = ["N.*_Q", "N.*_P", "N.*_f"]
 # regex_vzorec = ["N.*_u", "N.*_au", "N.*_i.*", "N.*_ai.*", "N.*_P.*", "N.*_Q.*", "N.*_f", "N.*_r"]
 for i, vzorec in enumerate(regex_vzorec):
     reg = re.compile(vzorec)
@@ -40,7 +39,7 @@ for i, vzorec in enumerate(regex_vzorec):
     plt_keys = list(filter(reg.match, data.keys()))
     plt.figure(i)
     for key in plt_keys:
-        plt.plot_date(time_arr, (data[key]-data[key][10])/np.amax(data[key]), '-', label=key)
-
+        plt.plot_date(time_arr, ((data[key] - data[key][0])), '-', label=key)
+        print(np.amax(data[key]))
     plt.legend()
 plt.show()
---
+++ jan/rezanje.py
@@ -0,0 +1,27 @@
+import numpy as np
+import re
+from branje import branje
+from iskanje import iskanje
+
+def rezanje(x):
+    data = branje(x)
+    delta_t = 1
+    cas_dogodka = iskanje(x)[1]
+    delta_i = delta_t * 50
+    i_dogodka = cas_dogodka * 50
+    new_data = dict()
+    
+    regex_vzorec = ["N.*_u", "N.*_au", "N.*_i.*", "N.*_ai.*", "N.*_P.*", "N.*_Q.*", "N.*_f", "N.*_r"]
+    for i, vzorec in enumerate(regex_vzorec):
+        reg = re.compile(vzorec)
+        keys = list(filter(reg.match, data.keys()))
+        for key in keys:
+            new_data[key] = np.ndarray(int(2 * delta_i - 1))
+            k = 0
+            for j in range(len(data[key])):
+                if abs(i_dogodka - j) < delta_i:
+                    new_data[key][k] = data[key][j]
+                    k = k + 1
+
+    return new_data
+        
