dd4fb7b0e79f5c0215d12af479f0deded0b300c2
opasni <opasni@users.noreply.github.com>
2017-10-07 11:57:51 +0200

    Update readdata.py

--- jaka/readdata.py
+++ jaka/readdata.py
@@ -15,31 +15,32 @@ with open("../Data/Simul/Sim1.csv") as dat_f:
 
 
 data = dict()
+time_arr = []
 for key in arr[0]:
-     data[key] = []
+     data[key] = np.zeros(len(arr))
 
-for line in arr:
+for i,line in enumerate(arr):
     for key in arr[0]:
         if key == 'Time':
-            data[key].append(datetime.strptime(line[key], '%Y-%m-%d %H:%M:%S.%f'))
+            time_arr.append(datetime.strptime(line[key], '%Y-%m-%d %H:%M:%S.%f'))
         else:
-            data[key].append(float(line[key]))
+            data[key][i] = float(line[key])
 
 
 # plt.format_xdata = mdates.DateFormatter('%Y-%m-%d %h-%m-%s.%')
 
 # plt_keys = ["N1_au", "N2_au", "N10_au", "N15_au"]
 
-dataN = np.asarray(data)
+
 regex_vzorec = ["N.*_u", "N.*_au", "N.*_r", "N.*_P.*", "N.*_Q.*"]
 # regex_vzorec = ["N.*_u", "N.*_au", "N.*_i.*", "N.*_ai.*", "N.*_P.*", "N.*_Q.*", "N.*_f", "N.*_r"]
 for i, vzorec in enumerate(regex_vzorec):
     reg = re.compile(vzorec)
 
-    plt_keys = list(filter(reg.match, dataN.keys()))
+    plt_keys = list(filter(reg.match, data.keys()))
     plt.figure(i)
     for key in plt_keys:
-        plt.plot_date(data['Time'], (data[key]-data[key][10])/np.amax(data[key]), '-', label=key)
+        plt.plot_date(time_arr, (data[key]-data[key][10])/np.amax(data[key]), '-', label=key)
 
     plt.legend()
 plt.show()
