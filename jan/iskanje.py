927dbfba52b7c4fb13f751fff8ff24fd787ceb8c
Brottod <jan@jan-laptop.localdomain>
2017-10-07 22:09:33 +0200

    iskanje dogodka

--- jan/iskanje.py
+++ jan/iskanje.py
@@ -2,7 +2,7 @@ import numpy as np
 from branje import branje
 import re
 
-def iskanje(x):
+def iskanje2(x):
     data = branje(x)
     maks = 0
     cas = 0
@@ -16,4 +16,42 @@ def iskanje(x):
                 cas = np.argmax(abs(data[key])) * 0.02
 
     return maks, cas
+
+def iskanje(x, N = 50, tol = 0.02):
+    data = branje(x)
+    maks = 0
+    cas = 0
+    mov_avgs = []
+    cumsum = [0]
+    vsota = np.zeros(len(data['Time']))
+    regex_vzorec = ["N.*_r"]
+    
+    for i, vzorec in enumerate(regex_vzorec):
+        reg = re.compile(vzorec)
+        keys = list(filter(reg.match, data.keys()))
+        for j in range(len(vsota)):
+            for key in keys:
+                vsota[j] = vsota[j] + data[key][j]
+
+    arr = []
+    cumsum = [0]
+    mov_avgs = []
+    i = 1
+    
+    while True:
+        cumsum.append(cumsum[i - 1] + vsota[i - 1])
+        if i >= N:
+            mov_avg = (cumsum[i] - cumsum[i - N] / N
+            mov_avgs.append(mov_avg)
+            if vsota[i + 1] > mov_avg and abs(vsota[i + 1] - mov_avg) > tol:
+                aktiven_dogodek = True
+                       # zacetek spikea, zacne delat array
+            if vsota[i + 1] < mov_avg and abs(vsota[i + 1] - mov_avg) > tol:
+                aktiven_dogodek = True
+                       # po vrhu, array se dela naprej
+            if abs(vsota[i + 1] - mov_avg) < tol and aktiven_dogodek = True:
+                aktiven_dogodek = False
+                return arr
+        i = i + 1
+            
 
