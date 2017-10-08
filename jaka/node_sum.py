a9c1c16658c9e276efda3c92bdc075364a0cc8b0
Jaka Sircelj <sircelj.jaka@gmail.com>
2017-10-08 10:42:42 +0200

    node_sum, sestevek v posameznem nodu in celotev

---
+++ jaka/node_sum.py
@@ -0,0 +1,60 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+import numpy as np
+import re
+
+arr = []
+sim = 2
+
+with open("../Data/Simul/Sim" + str(sim) + ".csv") as dat_f:
+    reader = csv.DictReader(dat_f, delimiter=";")
+    for row in reader:
+        arr.append(row)
+
+
+data = dict()
+time_arr = []
+for key in arr[0]:
+     data[key] = np.zeros(len(arr))
+
+for i,line in enumerate(arr):
+    for key in arr[0]:
+        if key == 'Time':
+            time_arr.append(datetime.strptime(line[key], '%Y-%m-%d %H:%M:%S.%f'))
+        else:
+            data[key][i] = float(line[key])
+
+
+vzorec = "N.*_P"
+reg = re.compile(vzorec)
+
+plt_keys = list(filter(reg.match, data.keys()))
+
+node_dict = dict()
+
+for key in plt_keys:
+    N_idx = int(re.findall(r'\d+', key)[0])
+    if N_idx in node_dict:
+        node_dict[N_idx] = np.add(node_dict[N_idx], data[key])
+    else:
+        node_dict[N_idx] = np.zeros(len(data["Time"]))
+        node_dict[N_idx] = np.add(node_dict[N_idx], data[key])
+
+print(plt_keys)
+
+for idx in node_dict:
+    plt.plot_date(time_arr, node_dict[idx], '-', label=idx)
+
+full_sum = np.zeros(len(data["Time"]))
+for idx in node_dict:
+    full_sum = np.add(full_sum, node_dict[idx])
+
+plt.plot_date(time_arr, full_sum, '-', linewidth=2, label="full")
+
+
+
+plt.title("sim" + str(sim))
+plt.legend()
+
+plt.show()
--- jaka/readdata.py
+++ jaka/readdata.py
@@ -37,7 +37,7 @@ for i,line in enumerate(arr):
 
 
 # regex_vzorec = ["N.*_u", "N.*_au", "N.*_r", "N.*_P.*", "N.*_Q.*"]
-regex_vzorec = ["N.*_u"]
+regex_vzorec = ["N.*_r"]
 # regex_vzorec = ["N.*_u", "N.*_au", "N.*_i.*", "N.*_ai.*", "N.*_P.*", "N.*_Q.*", "N.*_f", "N.*_r"]
 for i, vzorec in enumerate(regex_vzorec):
     reg = re.compile(vzorec)
@@ -50,7 +50,8 @@ for i, vzorec in enumerate(regex_vzorec):
 
     plt.figure(i)
     for key in plt_keys:
-        plt.plot_date(time_arr, (data[key]-data[key][10])/max_val, '-', label=key)
+        #plt.plot_date(time_arr, (data[key]-data[key][10])/max_val, '-', label=key)
+        plt.plot_date(time_arr, (data[key]) / np.average(data[key][10:500]), '-', label=key)
         # plt.plot_date(time_arr, data[key], '-', label=key)
 
 
