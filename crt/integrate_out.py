303116c9ad8ae0a79ab81c72a7aab80b0e6e6e8b
opasni <crthare@gmail.com>
2017-10-08 15:52:41 +0200

    My part

--- crt/integrate_out.py
+++ crt/integrate_out.py
@@ -16,7 +16,7 @@ def get_number(s, vzorec):
 def get_best_bets(sim):    
     data = rezanje(sim)
     sim_freq_tmp = []
-    #vzorec_all = ["N.*_P", "N.*_u", "N.*_au"]
+    #vzorec_all = ["N.*_P", "N.*_u", "N.*_au", "N.*_i"]
     #vzorec_all = ["N.*_P", "N.*_au", "N.*_i"]
     vzorec_all = ["N.*_au"]
     
@@ -36,19 +36,33 @@ def get_best_bets(sim):
         
         s = sorted(zip(plt_keys, freq_val), key=lambda x:-x[1])
         # sim_freq_tmp.append(plt_keys[np.argmax(abs(freq_val)])
-        sim_freq_tmp.append([(get_number(x[0], vzorec),x[1]) for x in s[:3]])
+        sim_freq_tmp.append([(x[0],get_number(x[0], vzorec),x[1]) for x in s])
     #sim_freq.append(sim_freq_tmp)
     return(sim_freq_tmp)
     
+def predict_nodes(predicts):
+    nodes_predict = []
+    for source in predicts:
+        #print (source)
+        if 'a' in source[0]:
+            nodes_predict.append(source[1:])
+        #elif 'P' in source[0]:
+        #    return 0
+        #else:
+        #    return source[0]
+    return nodes_predict
 
 final_data = []
 
-calculate
-
-for sim in range(1,3):
+for sim in range(1,24):
     print (sim)
     predict = get_best_bets(sim)
     for pre_sor in predict:
         #print(predict_nodes(pre_sor))
-        final_data.append(pre_sor)
-print (final_data)
+        final_data.append(predict_nodes(pre_sor))
+    #print (final_data)
+
+my_out_data = pd.DataFrame(final_data)
+
+my_out_data.to_csv('pointsrelations.csv', index=False, header=False)
+#print(sim_freq)
